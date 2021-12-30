dee8e9b3bece8dac1d1c03d808cdc4294e06fa92
Darrel Toledo <darreltoledo@icloud.com>
2021-12-30 07:03:44 +0000

    Added the button in header component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
     <Header />
-    <Button />
   </div>
 </template>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,17 @@
 <template>
   <header>
     <h1>Task Tracker</h1>
+    <Button />
   </header>
 </template>
 
 <script>
+import Button from './Button'
 export default {
-  name: 'Header'
+  name: 'Header',
+  components: {
+    Button
+  }
 }
 </script>
 
