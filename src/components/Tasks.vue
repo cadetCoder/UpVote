2a57808c66310204b798bcea1531c50545298045
Darrel Toledo <darreltoledo@icloud.com>
2022-01-01 21:50:33 +0000

    delete task method

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,19 +1,23 @@
 <template>
-  <div :key="task.id" v-for="task in Tasks">
-    <Task :task="task" />
+  <div :key="task.id" v-for="task in tasks">
+    <Task
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
+      @delete-task="$emit('delete-task', task.id)"
+      :task="task"
+    />
   </div>
 </template>
 
 <script>
-import Task from './Task.vue'
-
+import Task from './Task'
 export default {
   name: 'Tasks',
   props: {
-    tasks: Array
+    tasks: Array,
   },
   components: {
-    Task
-  }
+    Task,
+  },
+  emits: ['delete-task', 'toggle-reminder'],
 }
 </script>
