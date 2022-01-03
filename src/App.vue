8d8ff4cc2ca5a6fd134011d4a97c789d09bff038
Darrel Toledo <darreltoledo@icloud.com>
2022-01-03 21:19:09 +0000

    fetch task method in toggle reminder

--- src/App.vue
+++ src/App.vue
@@ -52,7 +52,22 @@ export default {
         res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting task')
       }
     },
-    toggleReminder(id) {
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+      const updTask = {
+        ...taskToToggle, reminder: !taskToToggle.reminder
+      }
+
+      const res = await fetch (`api/taks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updTask)
+      })
+
+      const data = await res.json()
+
       this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder } : task)
     },
     async fetchTasks() {
