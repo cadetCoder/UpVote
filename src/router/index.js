83f4d73114580cb2cb97f408a79f23f3e0ada25e
Darrel Toledo <darreltoledo@icloud.com>
2022-01-04 22:06:11 +0000

    defined routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,17 @@
-import {creatRouter, createWebHistroy} from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
+import About from '../views/About'
+
+const routes = [
+  { 
+    path: 'a/bout',
+    name: 'About',
+    component: About
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
